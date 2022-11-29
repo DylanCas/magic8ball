d51e4c425d18ff3d17ca4b42c6fd932bfdd7b30f
DylanCas <djcasabona12@gmail.com>
2022-11-28 18:14:17 -0700

    question is now answered on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,9 +28,9 @@ askBtn.addEventListener("click", function() {
     userQuestionEl.textContent = userName + " asks: " + userQuestionBox
   }
   userQuestionEl.style.display = "block"
-})
+  // generates random number to select answers
 
-// generates random number to select answers
+let answer = document.getElementById("questionAnswer")
 var randomNumber = Math.floor(Math.random() * 8);
 
 var eightBall = ""
@@ -38,35 +38,44 @@ var eightBall = ""
 switch (randomNumber) {
   case 0:
   eightBall = 'It is certain'
+  answer.textContent = eightBall
   console.log(eightBall)
   break;
   case 1:
   eightBall = 'It is decidedly so'
+  answer.textContent = eightBall
   console.log(eightBall)
   break;
   case 2:
   eightBall = 'Reply hazy try again'
+  answer.textContent = eightBall
   console.log(eightBall)
   break;
   case 3:
   eightBall = 'Cannot predict now'
+  answer.textContent = eightBall
   console.log(eightBall)
   break;
   case 4:
   eightBall = 'Do not count on it'
+  answer.textContent = eightBall
   console.log(eightBall)
   break;
   case 5:
   eightBall = 'My sources say no'
+  answer.textContent = eightBall
   console.log(eightBall)
   break;
   case 6:
   eightBall = 'Outlook not so good'
+  answer.textContent = eightBall
   console.log(eightBall)
   break;
   case 7:
   eightBall = 'Signs point to yes'
+  answer.textContent = eightBall
   console.log(eightBall)
   break;
   // I dont feel a default is needed here due to random numbers being used
 }
+})
